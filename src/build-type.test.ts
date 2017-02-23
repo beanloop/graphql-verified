b83532b738b191a2c9d2584a48ccd7fd183eb3ae
Jesper Håkansson <jesper@jesperh.se>
2017-02-23 11:30:36 +0100

    test: Add test for promise results in rules

--- src/build-type.test.ts
+++ src/build-type.test.ts
@@ -1,11 +1,15 @@
 /// <reference types="jest" />
-
+import 'regenerator-runtime/runtime'
 import {
+  graphql,
   GraphQLList,
+  GraphQLObjectType,
   GraphQLString,
+  GraphQLSchema,
 } from 'graphql'
 
 import {buildType} from './build-type'
+import {buildQuery} from './build-query'
 
 describe('build-type', () => {
   describe('buildType', () => {
@@ -29,6 +33,46 @@ describe('build-type', () => {
         readRules: false,
       })
       expect(builtType.graphQLInputType['_typeConfig'].fields.targetGroups).not.toBeUndefined()
+    })
+
+    it('should support promise results in rules', () => {
+      const builtType = buildType({
+        name: 'Type',
+        fields: {
+          prop: {type: GraphQLString},
+        },
+        writeRules: false,
+        readRules: {
+          prop: () => Promise.resolve(false)
+        },
+      })
+
+      const query = buildQuery('getProp', {
+        type: builtType,
+        resolve() {
+          return 'Hello'
+        }
+      })
+
+      const QueryType = new GraphQLObjectType({
+        name: 'Query',
+        fields: () => ({getProp: query}),
+      })
+
+      const schema = new GraphQLSchema({
+        query: QueryType,
+      })
+
+      return graphql(schema, `
+        query getProp {
+          getProp {
+            prop
+          }
+        }
+      `)
+        .then(result => {
+          expect(result).toEqual({data: {getProp: {prop: null}}})
+        })
     })
   })
 })
